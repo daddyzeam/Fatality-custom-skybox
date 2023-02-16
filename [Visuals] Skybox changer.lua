de3021c333756a10944e1c4ae8f4a7e78a66bcb8
zeam <125522189+daddyzeam@users.noreply.github.com>
2023-02-15 20:36:26 -0500

    Add files via upload

---
+++ [Visuals] Skybox changer.lua
@@ -0,0 +1,15 @@
+if fs.is_dir("csgo\\materials\\skybox") == false then -- Checks for custom skybox folder 
+    fs.create_dir("csgo\\materials\\skybox")
+end
+
+local skyboxes = {
+    "None",
+    "sky_lunacy",
+    "sky_aurora"
+  --"skybox name here" put skybox files in csgo\\materials\\skybox (if directory dosent exist, simply run the lua once then check the directory)
+}
+
+local skybox_combolist = gui.add_combo("Custom skybox", "lua>tab b", skyboxes)
+function on_paint()
+     cvar.sv_skyname:set_string(skyboxes[skybox_combolist:get_int() + 1]) -- Grabs current listbox index
+end
